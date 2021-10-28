adfe7359ebbbbee407c59aa75c0b9b667c6b04f1
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-10-28 19:32:08 +0000

    ultimos cambios de la version desktop

---
+++ js/video.js
@@ -0,0 +1,14 @@
+const videoPlayer = document.querySelector('.video-player')
+const video = videoPlayer.querySelector('.video')
+const playButton = videoPlayer.querySelector('.play-button')
+
+//Play
+playButton.addEventListener('click', (e) => {
+    if(video.paused){
+        video.play()
+        e.target.textContent = '| |'
+    }else {
+        video.pause()
+        e.target.textContent = 'play'
+    }
+})
