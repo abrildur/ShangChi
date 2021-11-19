ef63848a3b5b112eaa101e4533a12d8f08bcebf0
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-11-19 03:29:16 +0000

    pruebas en java

--- tutorial/src/App.java
+++ tutorial/src/App.java
@@ -1,14 +1,45 @@
+/*
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+ 
+Test Cases:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
 import java.util.Scanner;
+
+*/
+
 
 public class App {
     
+    public int lenghtofWords(String s){
+            if( s == null) {
+                return 0;
+            }
+            String[] words = s.split(" ");
+            int len = 0;
+
+            for (int i = words.length - 1; i >= 0; i--){
+                len = words [i].length();
+                if (len > 0 ) {
+                    break;
+                }
+            }
+            return len;
+        }
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-        double numero;
-
-        System.out.print("Digite un numero: ");
-        numero = entrada.nextDouble();
-
-        System.out.println("El numero es: " + numero);
+        String input = "luffy is still joyboy";
+        App holi = new App();
+        System.out.println("The lenght of the last world is: " + holi.lenghtofWords(input));
     }
 }
---
+++ tutorial/src/Palindrome.java
@@ -0,0 +1,27 @@
+/* 
+Given an integer x, return true if x is a palindrome integer.
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+ 
+Test Cases:
+Input: x = 121
+Output: true
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Input: x = -101
+Output: false
+ 
+*/
+
+
+public class Palindrome {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
